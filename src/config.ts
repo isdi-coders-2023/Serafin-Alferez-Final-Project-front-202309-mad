167d279f94b8e4c748fdcafd0ae0f5110b8c2ea7
Serafin Alferez <malaga.carbon3@gmail.com>
2023-12-04 16:33:45 +0100

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const serverUrl =
+  location.hostname === 'localhost'
+  ? 'http://localhost:2700'
+  : 'https://render';
---
+++ src/types/img.data.ts
@@ -0,0 +1,8 @@
+export type ImgData = {
+  publicId: string;   // Es el nombre de la imagen tal cual la guarda Cloudinary
+  size: number;
+  width: number;
+  height: number;
+  format: string;
+  url: string;
+};
