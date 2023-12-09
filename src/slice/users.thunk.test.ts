d26cedd1cb2992360494735dc59df5f2cede37a7
Serafin Alferez <malaga.carbon3@gmail.com>
2023-12-09 21:36:12 +0100

    Add thunk test

---
+++ src/slice/users.thunk.test.ts
@@ -0,0 +1,43 @@
+import '@testing-library/jest-dom';
+
+
+
+import { UserRepo } from '../services/api.repo.users';
+
+import { loginThunk } from './users.thunk';
+import { LoginUser } from '../entities/user';
+import { store } from '../store/store';
+
+
+
+
+describe('Give user.thunk', () => {
+  describe('When an action is taken ', () => {
+    const userTest = {
+      repo : {
+        login: jest.fn().mockReturnValue({
+          token: '',
+        }),
+        loginWithToken: jest.fn().mockReturnValue({
+          token: '',
+        }),
+      } as unknown as UserRepo,
+      
+  
+    };
+
+    test('Then is should dispatch loginThunk and update user store', async () => {
+      const userData = { ...userTest, loginUser: {} as LoginUser };
+      await store.dispatch(loginThunk(userData));
+      expect(userData.repo.login).toHaveBeenCalled();
+
+    })
+
+  })
+
+
+});
+
+    
+
+
